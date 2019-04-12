3057384c093a1226e0c692e3d497beb4ac6f93dc
U-NSN-INTRA\rding <myfreegithub@gmail.com>
2019-04-12 17:23:49 +0800

    Rearrange the directory of BasicKnowledge

---
+++ BasicKnowledge/parameter_deduce_demo/ParameterDeduceDemo.cpp
@@ -0,0 +1,238 @@
+#include <type_traits>
+#include <iostream>
+#include <boost/type_index.hpp>
+
+using std::cout;
+using std::endl;
+
+// Simulate auto param = value; Simplify the code writing by no need to define different variables
+auto checkAutoDecduction = [](auto param){
+    using Type = decltype(param);
+    using boost::typeindex::type_id_with_cvr;
+    cout << "param's type is: "<< type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+};
+
+// Simulate const auto param = value;
+auto checkConstAutoDecduction = [](const auto param){
+    using Type = decltype(param);
+    using boost::typeindex::type_id_with_cvr;
+    cout << "param's type is: "<< type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+};
+
+// Simulate auto& param = value;
+auto checkAutoReferenceDecduction = [](auto& param){
+    using Type = decltype(param);
+    using boost::typeindex::type_id_with_cvr;
+    cout << "param's type is: "<< type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+};
+
+// Simulate const auto& param = value;
+auto checkConstAutoReferenceDecduction = [](const auto& param){
+    using Type = decltype(param);
+    using boost::typeindex::type_id_with_cvr;
+    cout << "param's type is: "<< type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+};
+
+template<typename T>
+void checkTemplateTypeAndParamType(T param)
+{
+    using boost::typeindex::type_id_with_cvr;
+    cout << "T is " << type_id_with_cvr<T>().pretty_name();
+    cout << "; param's type is: " << type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+}
+
+template<typename T>
+void checkTemplateReferenceAndParamType(T& param)
+{
+    using boost::typeindex::type_id_with_cvr;
+    cout << "T is " << type_id_with_cvr<T>().pretty_name();
+    cout << "; param's type is: " << type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+}
+
+template<typename T>
+void checkTemplateConstReferenceAndParamType(const T& param)
+{
+    using boost::typeindex::type_id_with_cvr;
+    cout << "T is " << type_id_with_cvr<T>().pretty_name();
+    cout << "; param's type is: " << type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+}
+
+void showInputs()
+{
+    cout << "int i = 10;" << endl
+         << "int& ri = i;" << endl
+         << "const int ci = 10;" << endl
+         << "const int& cri = ci;" << endl;
+}
+
+// show type of variable's type which is defined by:
+// auto param = value;
+void showAutoDeductionResult()
+{
+    int i = 10;
+    int& ri = i;
+    const int ci = 10;
+    const int& cri = ci;
+
+    cout << "auto param = i;   // ";
+    checkAutoDecduction(i);
+    cout << "auto param = 10;  // ";
+    checkAutoDecduction(10);
+    cout << "auto param = ci;  // ";
+    checkAutoDecduction(ci);
+    cout << "auto param = ri;  // ";
+    checkAutoDecduction(ri);
+    cout << "auto param = cri; // ";
+    checkAutoDecduction(cri);
+}
+
+// show type of variable's type which is defined by:
+// const auto param = value;
+void showConstAutoDeductionResult()
+{
+    int i = 10;
+    int& ri = i;
+    const int ci = 10;
+    const int& cri = ci;
+
+    cout << "const auto param = i;   // ";
+    checkConstAutoDecduction(i);
+    cout << "const auto param = 10;  // ";
+    checkConstAutoDecduction(10);
+    cout << "const auto param = ci;  // ";
+    checkConstAutoDecduction(ci);
+    cout << "const auto param = ri;  // ";
+    checkConstAutoDecduction(ri);
+    cout << "const auto param = cri; // ";
+    checkConstAutoDecduction(cri);
+}
+
+// show type of variable's type which is defined by:
+// auto& param = value;
+void showAutoReferenceDecductionResult()
+{
+    int i = 10;
+    int& ri = i;
+    const int ci = 10;
+    const int& cri = ci;
+
+    cout << "auto& param = i;   // ";
+    checkAutoReferenceDecduction(i);
+    // We cannot assign an RValue to a reference
+    cout << "auto& param = 10;  // compiling will fail here" << endl;
+    cout << "auto& param = ci;  // ";
+    checkAutoReferenceDecduction(ci);
+    cout << "auto& param = ri;  // ";
+    checkAutoReferenceDecduction(ri);
+    cout << "auto& param = cri; // ";
+    checkAutoReferenceDecduction(cri);
+}
+
+// show type of variable's type which is defined by:
+// const auto& param = value;
+void showConstAutoReferenceDecductionResult()
+{
+    int i = 10;
+    int& ri = i;
+    const int ci = 10;
+    const int& cri = ci;
+
+    showInputs();
+    cout << "const auto& param = i;   // ";
+    checkConstAutoReferenceDecduction(i);
+    cout << "const auto& param = 10;  //";
+    checkConstAutoReferenceDecduction(10);
+    cout << "const auto& param = ci;  // ";
+    checkConstAutoReferenceDecduction(ci);
+    cout << "const auto& param = ri;  // ";
+    checkConstAutoReferenceDecduction(ri);
+    cout << "const auto& param = cri; // ";
+    checkConstAutoReferenceDecduction(cri);
+}
+
+void showTempalateParameterDeduction()
+{
+    int i = 10;
+    int& ri = i;
+    const int ci = 10;
+    const int& cri = ci;
+
+    cout << "template<typename T>" << endl;
+    cout << "void func(T param);" << endl;
+
+    cout << "func(i);   // ";
+    checkTemplateTypeAndParamType(i);
+    cout << "func(10);  // ";
+    checkTemplateTypeAndParamType(10);
+    cout << "func(ci);  // ";
+    checkTemplateTypeAndParamType(ci);
+    cout << "func(ri);  // ";
+    checkTemplateTypeAndParamType(ri);
+    cout << "func(cri); // ";
+    checkTemplateTypeAndParamType(cri);
+}
+
+void showTempalateReferenceParameterDeduction()
+{
+    int i = 10;
+    int& ri = i;
+    const int ci = 10;
+    const int& cri = ci;
+
+    cout << "template<typename T>" << endl;
+    cout << "void func(T& param);" << endl;
+
+    cout << "func(i);   // ";
+    checkTemplateReferenceAndParamType(i);
+    cout << "func(10);  // Compile will failed here, due to cannot assign a RValue to reference" << endl;
+    cout << "func(ci);  // ";
+    checkTemplateReferenceAndParamType(ci);
+    cout << "func(ri);  // ";
+    checkTemplateReferenceAndParamType(ri);
+    cout << "func(cri); // ";
+    checkTemplateReferenceAndParamType(cri);
+}
+
+void showTempalateConstReferenceParameterDeduction()
+{
+    int i = 10;
+    int& ri = i;
+    const int ci = 10;
+    const int& cri = ci;
+
+    cout << "template<typename T>" << endl;
+    cout << "void func(const T& param);" << endl;
+
+    cout << "func(i);   // ";
+    checkTemplateConstReferenceAndParamType(i);
+    cout << "func(10);  // ";
+    checkTemplateConstReferenceAndParamType(10);
+    cout << "func(ci);  // ";
+    checkTemplateConstReferenceAndParamType(ci);
+    cout << "func(ri);  // ";
+    checkTemplateConstReferenceAndParamType(ri);
+    cout << "func(cri); // ";
+    checkTemplateConstReferenceAndParamType(cri);
+}
+
+int main(int, char**)
+{
+    showInputs();
+    cout << endl;
+    showAutoDeductionResult();
+    cout << endl;
+    showConstAutoDeductionResult();
+    cout << endl;
+    showAutoReferenceDecductionResult();
+    cout << endl;
+    showConstAutoReferenceDecductionResult();
+    cout << endl;
+
+    showTempalateParameterDeduction();
+    cout << endl;
+    showTempalateReferenceParameterDeduction();
+    cout << endl;
+    showTempalateConstReferenceParameterDeduction();
+    cout << endl;
+    return 1;
+}
---
+++ BasicKnowledge/type_traits_demo/TypeTraitsDemo.cpp
@@ -0,0 +1,11 @@
+#include <type_traits>
+#include <iostream>
+#include <boost/type_index.hpp>
+
+using std::cout;
+using std::endl;
+
+int main(int, char**)
+{
+    return 1;
+}
